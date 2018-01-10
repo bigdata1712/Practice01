07463462c04f1024607e055dc78ac47b6dee3024
BIT-USER <BIT-USER@DESKTOP-6G8H6K0>
2018-01-10 09:48:52 +0900

    풀이

---
+++ src/com/javaex/practice/Problem04.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Problem04 {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=10 ; i++){
+			
+			for(int j=i; j<i+10 ; j++){
+				System.out.print(j + "\t");
+			}	
+			System.out.println("");
+		}
+		
+	}
+
+}
